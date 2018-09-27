93005e64605151076cdb43bca0d6759a228e2311
kathi <kathi@Sagar>
2018-09-27 23:09:59 +0530

    update the case

--- com.crm.module/src/main/java/com/crm/module/testcases/SampleTestcase.java
+++ com.crm.module/src/main/java/com/crm/module/testcases/SampleTestcase.java
@@ -2,8 +2,9 @@ package com.crm.module.testcases;
 
 public class SampleTestcase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
 
 	}
 
