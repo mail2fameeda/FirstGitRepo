365a1deaeecf3887d5815d766689f54383ca75b2
kathi <kathi@Sagar>
2018-09-27 22:29:41 +0530

    First Commit

---
+++ com.crm.module/src/main/java/com/crm/module/testcases/Precondition.java
@@ -0,0 +1,10 @@
+package com.crm.module.testcases;
+
+public class Precondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com.crm.module/src/main/java/com/crm/module/webpages/Login_POM.java
@@ -0,0 +1,10 @@
+package com.crm.module.webpages;
+
+public class Login_POM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
